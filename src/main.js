58e6941ac3a6de06f7229f2fd5840e2b0e18bade
wumih <120448608+wumih@users.noreply.github.com>
2025-11-28 01:36:24 +0800

    feat: <完成了基本配置，开始写路由>

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import { Button, Icon } from 'vant'
+// 导入按需导入的配置文件
+import '@/utils/vant-ui'
 
+Vue.use(Button)
+Vue.use(Icon)
 Vue.config.productionTip = false
 
 new Vue({
---
+++ src/utils/vant-ui.js
@@ -0,0 +1,4 @@
+import { Button, Icon } from 'vant'
+import Vue from 'vue'
+Vue.use(Button)
+Vue.use(Icon)
